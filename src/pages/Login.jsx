d86f0bf3765d235c7f7ca1bffc5ebed49ec4fd91
JP <dawrkshado@gmail.com>
2025-11-15 13:24:34 +0800

    forgot password update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 function Login() {
   return (
     <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
+      className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat h-[100vh] w-[100vw] relative"
       style={{ backgroundImage: `url(${Bg})` }}
     >
      <div className="translate-y-26"> 
     <Form route="/api/token/" method="login" />
   </div>
-          <div className="absolute top-125  2xl:top-170 text-blue-500  w-fit ">
+          <div className="absolute top-125  2xl:top-[70%] text-blue-500  w-fit ">
     <Link to="/forgotPassword">
       <p className="hover:underline">Forgot Password?</p>
     </Link>
